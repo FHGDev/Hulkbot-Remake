0bc09da77d7a0ce4ac17d077ba60a6219406ef29
FHGDev <gideon.lingle1@gmail.com>
2018-11-21 21:12:57 -0500

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -4,6 +4,19 @@ const Money = require('../models/money.js')
 module.exports.execCmd = (bot, message, args) => {
   const randomcash = Math.ceil(Math.random() * 300)
   Money.findOne({userID: message.author.id}, (err, data) => {
+    if (!data) {
+      const newMoneyz = new Money({
+        userID: message.author.id,
+        money: randomcash
+      })
+      em
+      .setTitle("You worked at the office today!")
+      .setDescription(`Earnings: **${randomcash}** -------- Balance: ${newMoneyz.money}`)
+      .setTimestamp()
+      .setColor("GREEN")
+      message.channel.send({embed: em})
+      newMoneyz.save()
+    }
     data.money = data.money + randomcash
     data.save()
     em
