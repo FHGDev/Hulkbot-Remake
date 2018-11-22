a70d2fbae1f5e312310e2923c84d99711edb06fe
FHGDev <gideon.lingle1@gmail.com>
2018-11-21 20:08:22 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const discord = require('discord.js')
+const bot = new discord.Client()
+const prefix = "h:"
+bot.commands = new discord.Collection()
+
+require('fs').readdir('./commands/', (err, files) => {
+   if (err) return console.log("error loading commands!");
+   files.filter(f => f.split(".").pop() == "js").forEach((f,i) => {
+      bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+   })
+})
